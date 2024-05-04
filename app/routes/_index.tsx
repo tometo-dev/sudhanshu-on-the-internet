ca16c070b9342838abd048f3d52e5991101a7211
Sudhanshu <perfectsudh@gmail.com>
2024-05-04 13:37:36 +0530

    fix(meta): update title, description and favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,39 +2,18 @@ import type { MetaFunction } from "@remix-run/cloudflare";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Sudhanshu's Portfolio" },
     {
       name: "description",
-      content: "Welcome to Remix! Using Vite and Cloudflare!",
+      content: "Sudhanshu's developer portfolio website",
     },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix (with Vite and Cloudflare)</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-            className="text-blue-500"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <h1>Welcome to Sudhanshu&apos;s Portfolio</h1>
     </div>
   );
 }
