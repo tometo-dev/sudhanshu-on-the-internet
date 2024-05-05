35e5fb07c52a1593255cd80e8122f7f785739e12
Sudhanshu <perfectsudh@gmail.com>
2024-05-05 12:54:02 +0530

    add blog footer

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -2,8 +2,22 @@ import { Outlet } from "@remix-run/react";
 
 export default function Blog() {
   return (
-    <div className="prose h-full w-full max-w-full [&_a]:text-accent [&_a]:no-underline hover:[&_a]:underline focus:[&_a]:rounded focus:[&_a]:outline-none focus:[&_a]:ring-[1px] focus:[&_a]:ring-accent focus:[&_a]:ring-offset-2">
-      <Outlet />
+    <div className="flex h-full w-full flex-col">
+      <div className="prose max-w-full flex-1 [&_a]:text-accent [&_a]:no-underline hover:[&_a]:underline focus:[&_a]:rounded focus:[&_a]:outline-none focus:[&_a]:ring-[1px] focus:[&_a]:ring-accent focus:[&_a]:ring-offset-2">
+        <Outlet />
+      </div>
+      <aside className="pt-4">
+        If you like what you read, consider following me on{" "}
+        <a
+          href="https://twitter.com/tsuki42_"
+          target="_blank"
+          rel="noreferrer"
+          className="text-accent hover:underline focus:rounded focus:outline-none focus:ring-[1px] focus:ring-accent focus:ring-offset-2"
+        >
+          Twitter
+        </a>
+        .
+      </aside>
     </div>
   );
 }
