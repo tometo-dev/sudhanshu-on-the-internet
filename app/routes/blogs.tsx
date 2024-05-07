bc41281391b6847885e94d35f934adc817277a2a
Sudhanshu <perfectsudh@gmail.com>
2024-05-07 18:38:44 +0530

    (blog): add `weird-google-autocomplete` blog

--- app/routes/blogs.tsx
+++ app/routes/blogs.tsx
@@ -16,11 +16,11 @@ function BlogCard({
     <Link
       to={link}
       className={cn(
-        "w-full rounded-lg border border-secondary focus:border-none focus:outline-none focus:ring focus:ring-accent md:w-1/2 lg:w-1/3",
+        "h-full w-full rounded-lg border border-secondary shadow-lg hover:border-accent focus:border-none focus:outline-none focus:ring focus:ring-accent md:w-1/2 lg:w-1/3",
         className,
       )}
     >
-      <div className="p-4 text-primary shadow-lg hover:border-accent  ">
+      <div className="flex h-full flex-col gap-4 p-4 text-primary">
         <h2 className="text-xl font-bold">{title}</h2>
         <p className="text-sm text-secondary ">{description}</p>
       </div>
@@ -33,11 +33,18 @@ export default function Blogs() {
     <div>
       In this small corner of the internet, I write about things that interest
       me. Here are some of my posts:
-      <div className="flex h-full w-full flex-wrap py-4">
+      <div className="flex h-full w-full flex-wrap gap-4 py-4">
         <BlogCard
           link="/blog/config-ui-intro"
           title="Config-based UI"
           description="Introduction to what config-based UI is and why it's useful."
+          className="min-h-[12rem]"
+        />
+        <BlogCard
+          link="/blog/weird-google-autocomplete"
+          title="Weird Google Autocomplete + MUI"
+          description="A day of my life I spent trying to make Google Autocomplete work well with Material UI."
+          className="min-h-[12rem]"
         />
       </div>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
 export default {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
