0a90dca568a087dc19b1de595444fcebad900f1c
Sudhanshu <perfectsudh@gmail.com>
2024-05-17 08:29:31 +0530

    feat: add <ClientOnly> component

---
+++ app/components/client-only.tsx
@@ -0,0 +1,64 @@
+/**
+ * Source code: https://github.com/sergiodxa/remix-utils/blob/main/src/react/client-only.tsx
+ */
+
+import { useSyncExternalStore } from "react";
+
+function subscribe() {
+  // biome-ignore lint/suspicious/noEmptyBlockStatements: Mock function
+  return () => {};
+}
+
+/**
+ * Return a boolean indicating if the JS has been hydrated already.
+ * When doing Server-Side Rendering, the result will always be false.
+ * When doing Client-Side Rendering, the result will always be false on the
+ * first render and true from then on. Even if a new component renders it will
+ * always start with true.
+ *
+ * Example: Disable a button that needs JS to work.
+ * ```tsx
+ * let hydrated = useHydrated();
+ * return (
+ *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
+ *     Click me
+ *   </button>
+ * );
+ * ```
+ */
+function useHydrated() {
+  return useSyncExternalStore(
+    subscribe,
+    () => true,
+    () => false,
+  );
+}
+
+export type ClientOnlyProps = {
+  /**
+   * You are encouraged to add a fallback that is the same dimensions
+   * as the client rendered children. This will avoid content layout
+   * shift which is disgusting
+   */
+  children(): React.ReactNode;
+  fallback?: React.ReactNode;
+};
+
+/**
+ * Render the children only after the JS has loaded client-side. Use an optional
+ * fallback component if the JS is not yet loaded.
+ *
+ * Example: Render a Chart component if JS loads, renders a simple FakeChart
+ * component server-side or if there is no JS. The FakeChart can have only the
+ * UI without the behavior or be a loading spinner or skeleton.
+ * ```tsx
+ * return (
+ *   <ClientOnly fallback={<FakeChart />}>
+ *     {() => <Chart />}
+ *   </ClientOnly>
+ * );
+ * ```
+ */
+export function ClientOnly({ children, fallback = null }: ClientOnlyProps) {
+  return useHydrated() ? <>{children()}</> : <>{fallback}</>;
+}
