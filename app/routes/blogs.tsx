6b38e1d78905fe53237ae83d2189fd6cd7b3ce51
Sudhanshu <perfectsudh@gmail.com>
2024-08-31 17:43:56 +0530

    feat(/blogs): sort blog listing by authored date

--- app/routes/blogs.tsx
+++ app/routes/blogs.tsx
@@ -1,28 +1,32 @@
 import { Link, useLoaderData } from "@remix-run/react";
 import { cn } from "~/lib/utils";
+import { parse } from "date-fns/parse";
 
 function BlogCard({
+  link,
   title,
   description,
+  date,
   className,
-  link,
 }: {
+  link: string;
   title: string;
   description: string;
-  link: string;
+  date: string;
   className?: string;
 }) {
   return (
     <Link
       to={link}
       className={cn(
-        "h-full w-full rounded-lg border border-secondary shadow-lg hover:border-accent focus:border-none focus:outline-none focus:ring focus:ring-accent md:w-1/2 lg:w-1/3",
+        "w-full rounded-lg border border-secondary shadow-lg hover:border-accent focus:border-none focus:outline-none focus:ring focus:ring-accent md:w-1/2 lg:w-1/3",
         className,
       )}
     >
       <div className="flex h-full flex-col gap-4 p-4 text-primary">
         <h2 className="text-xl font-bold">{title}</h2>
         <p className="text-sm text-secondary">{description}</p>
+        <p className="mt-auto pt-2 text-sm text-accent">{date}</p>
       </div>
     </Link>
   );
@@ -36,15 +40,29 @@ export const loader = () => {
         import: "frontmatter",
         eager: true,
       }),
-    ).map(([filePath, contents]) => {
-      return [
-        filePath
-          .replace("./", "/")
-          .replace(".", "/")
-          .replace(/\.mdx$/, ""),
-        contents,
-      ];
-    }),
+    )
+      .map(([filePath, contents]) => {
+        return [
+          filePath
+            .replace("./", "/")
+            .replace(".", "/")
+            .replace(/\.mdx$/, ""),
+          contents,
+        ];
+      })
+      .sort(([, aContent], [, bContent]) => {
+        // Sort by date in descending order
+        try {
+          // @ts-expect-error - we know the shape of the content
+          const aDate = parse(aContent.date, "MMM dd, yyyy", new Date());
+          // @ts-expect-error - we know the shape of the content
+          const bDate = parse(bContent.date, "MMM dd, yyyy", new Date());
+
+          return bDate.getTime() - aDate.getTime();
+        } catch (e) {
+          return 0;
+        }
+      }),
   ) as Record<
     string,
     {
@@ -77,12 +95,13 @@ export default function Blogs() {
       In this small corner of the internet, I write about things that interest
       me. Here are some of my posts:
       <div className="flex h-full w-full flex-wrap gap-4 py-4">
-        {Object.entries(data).map(([slug, { title, description }]) => (
+        {Object.entries(data).map(([slug, { title, description, date }]) => (
           <BlogCard
             key={slug}
             link={slug}
             title={title}
             description={description}
+            date={date}
             className="min-h-[12rem]"
           />
         ))}
